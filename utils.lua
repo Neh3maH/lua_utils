438190384d84ccbf5ea8ec24a3b8402ca6038fad
Shapillon <sasha.fungoid@gmail.com>
2025-06-01 22:49:25 +0200

    Fix loader

--- utils.lua
+++ utils.lua
@@ -1,21 +1,29 @@
-local lib = function (pwd)
-	local old_path = package.path
-	package.path = pwd .. '/src/?.lua;' .. package.path
+local lib = {}
 
-	lib = {}
-	lib.misc = require 'misc'
-	for k, v in pairs(lib.misc) do
-		lib[k] = v
+if not __UTILS__LIB__LOCATION__ then
+	print("UNSET")
+	return function(pwd)
+		__UTILS__LIB__LOCATION__ = pwd
 	end
-
-	lib.shell = shell or require 'shell'
-	lib.collections = require 'collections'
-	lib.types = require 'types'
-	lib.fs = fs or require 'fs'
-	lib.misc = misc
-
-	package.path = old_path
-	return lib
 end
+
+local function pwd()
+	return __UTILS__LIB__LOCATION__
+end
+
+package.path = pwd() .. '/src/?.lua;' .. package.path
+
+lib.misc = require 'misc'
+for k, v in pairs(lib.misc) do
+	lib[k] = v
+end
+
+lib.shell = shell or require 'shell'
+lib.collections = require 'collections'
+lib.types = require 'types'
+lib.fs = fs or require 'fs'
+lib.misc = misc
+
+print(type(lib), tmp)
 
 return lib
